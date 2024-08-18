0260a294ee603cd82d84eee177803af015a86efd
spektrsoyuz <christies1109@gmail.com>
2024-08-18 00:09:43 -0400

    Initial upload to repo

---
+++ frames.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+"""
+File name: frames.py
+Author: Seth Christie
+Created: 2024-08-17
+Version: 1.0
+"""
+from pathlib import Path
+
+from PyQt6.QtGui import QFont
+from PyQt6.QtWidgets import QLabel, QGridLayout, QLineEdit, QPushButton, QSpacerItem, QFileDialog, QGroupBox
+
+
+# ----------------------------------------------------- classes --------------------------------------------------------
+
+class ScheduleFrame(QGroupBox):
+    def __init__(self, parent, title="Schedule Options"):
+        super().__init__(parent)
+        self.layout = QGridLayout(self)
+        self.layout.setContentsMargins(10, 20, 10, 20)
+        self.font = QFont('Helvetica', 12)
+        self.setTitle(title)
+        self.setFont(QFont('Helvetica', 10))
+
+        # url label
+        self.url_label = QLabel(parent=self, text="Schedule URL: ")
+        self.url_label.setFont(self.font)
+        self.layout.addWidget(self.url_label, 0, 0)
+
+        # url input
+        self.url_input = QLineEdit(parent=self)
+        self.url_input.setFont(self.font)
+        self.url_input.setText("https://docs.google.com/spreadsheets/")
+        self.url_input.textEdited.connect(self.clear_labels)
+        self.layout.addWidget(self.url_input, 0, 1)
+
+        # sheet name label
+        self.sheet_label = QLabel(parent=self, text="Sheet Name: ")
+        self.sheet_label.setFont(self.font)
+        self.layout.addWidget(self.sheet_label, 1, 0)
+
+        # sheet name input
+        self.sheet_input = QLineEdit(parent=self)
+        self.sheet_input.setFont(self.font)
+        self.sheet_input.setText("Sheet1")
+        self.sheet_input.textEdited.connect(self.clear_labels)
+        self.layout.addWidget(self.sheet_input, 1, 1)
+
+        # add spacer
+        self.layout.addItem(QSpacerItem(120, 20), 2, 0)
+
+        # export button
+        self.export_button = QPushButton(parent=self, text="Export")
+        self.export_button.setFont(self.font)
+        self.export_button.setFixedWidth(90)
+        self.layout.addWidget(self.export_button, 3, 0)
+
+        # export status label
+        self.export_label = QLabel(parent=self, text="")
+        self.export_label.setFont(self.font)
+        self.layout.addWidget(self.export_label, 3, 1, 1, 2)
+
+        # validate button
+        self.validate_button = QPushButton(parent=self, text="Validate")
+        self.validate_button.setFont(self.font)
+        self.validate_button.setFixedWidth(90)
+        self.layout.addWidget(self.validate_button, 4, 0)
+
+        # validate status label
+        self.validate_label = QLabel(parent=self, text="")
+        self.validate_label.setFont(self.font)
+        self.layout.addWidget(self.validate_label, 4, 1, 1, 2)
+
+    def clear_labels(self):
+        self.export_label.setText("")
+        self.validate_label.setText("")
+
+
+class DegreeworksFrame(QGroupBox):
+    def __init__(self, parent, title="DegreeWorks Options"):
+        super().__init__(parent)
+        self.layout = QGridLayout(self)
+        self.layout.setContentsMargins(10, 20, 10, 20)
+        self.font = QFont('Helvetica', 12)
+        self.setTitle(title)
+        self.setFont(QFont('Helvetica', 10))
+
+        # file label
+        self.file_label = QLabel(parent=self, text="Select File: ")
+        self.file_label.setFont(self.font)
+        self.layout.addWidget(self.file_label, 0, 0)
+
+        # file input
+        self.file_input = QLineEdit(parent=self)
+        self.file_input.setFont(self.font)
+        self.file_input.textEdited.connect(self.clear_labels)
+        self.layout.addWidget(self.file_input, 0, 1)
+
+        # file browse button
+        self.file_button = QPushButton(parent=self, text="Browse")
+        self.file_button.setFont(self.font)
+        self.file_button.clicked.connect(self.browse_file)
+        self.file_button.clicked.connect(self.clear_labels)
+        self.layout.addWidget(self.file_button, 0, 2)
+
+        # add spacer
+        self.layout.addItem(QSpacerItem(120, 20), 1, 0)
+
+        # create button
+        self.create_button = QPushButton(parent=self, text="Create")
+        self.create_button.setFont(self.font)
+        self.create_button.setFixedWidth(90)
+        self.create_button.clicked.connect(self.create_schedule)
+        self.layout.addWidget(self.create_button, 2, 0)
+
+        # create status label
+        self.create_label = QLabel(parent=self, text="")
+        self.create_label.setFont(self.font)
+        self.layout.addWidget(self.create_label, 2, 1, 1, 2)
+
+    def browse_file(self):
+        filename, _ = QFileDialog.getOpenFileName(
+            self, "Select PDF File", "", "PDF files (*.pdf)"
+        )
+        if filename:
+            self.file_input.setText(filename)
+            self.create_label.setText("")
+
+    def create_schedule(self):
+        filename = self.file_input.text()
+        self.create_label.setStyleSheet("color: rgb(179, 235, 242);")
+        self.create_label.setText("Creating Schedule...")
+
+        if filename == "":
+            self.create_label.setStyleSheet("color: rgb(255, 105, 97);")
+            self.create_label.setText("Please select a file.")
+            return
+
+        if not Path(filename).is_file():
+            self.create_label.setStyleSheet("color: rgb(255, 105, 97);")
+            displayed_name = filename
+            if len(filename) > 32:
+                displayed_name = displayed_name[:32] + "..."
+            self.create_label.setText("File " + displayed_name + " was not found.")
+            return
+
+        # TODO create schedule functionality
+
+        self.create_label.setStyleSheet("color: rgb(119, 221, 119);")
+        self.create_label.setText("Schedule created!")
+
+    def clear_labels(self):
+        self.create_label.setText("")
---
+++ main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+"""
+File name: main.py
+Author: Seth Christie
+Created: 2024-08-17
+Version: 1.0
+"""
+import sys
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QVBoxLayout, QSpacerItem
+from PyQt6.QtGui import QFont
+import qdarktheme
+
+import frames
+
+
+# ----------------------------------------------------- classes --------------------------------------------------------
+
+class Application(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.create_window()
+        self.layout = QVBoxLayout()
+
+        # create title widget
+        self.title_font = QFont('Helvetica', 24)
+        self.title_font.setBold(True)
+
+        self.title_label = QLabel(parent=self, text="Scheduler Tool")
+        self.title_label.setFont(QFont('Helvetica', 24))
+        self.title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.layout.addWidget(self.title_label)
+
+        # add frames to layout
+        self.schedule_frame = frames.ScheduleFrame(self)
+        self.layout.addWidget(self.schedule_frame)
+
+        self.layout.addItem(QSpacerItem(0, 20))
+
+        self.degreeworks_frame = frames.DegreeworksFrame(self)
+        self.layout.addWidget(self.degreeworks_frame)
+
+        self.layout.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignCenter)  # set alignment of layout
+        self.setLayout(self.layout)  # set layout of widget
+
+    def create_window(self):
+        """Creates the main window."""
+        self.resize(600, 800)
+        self.setFixedSize(600, 800)
+        self.setWindowTitle("Scheduler Tool")
+
+        self.setPalette(qdarktheme.load_palette('dark'))
+        self.setStyleSheet(qdarktheme.load_stylesheet())
+
+    def center(self):
+        """Centers the window on the screen."""
+        qt = self.frameGeometry()
+        cp = self.screen().availableGeometry().center()
+        qt.moveCenter(cp)
+        self.move(qt.topLeft())
+
+
+# ---------------------------------------------------- functions -------------------------------------------------------
+
+def main():
+    app = QApplication(sys.argv)  # create application
+    ex = Application()
+    ex.show()  # show window
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
